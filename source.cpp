a87e2c6038d1368fa82619c7629d391f60cae0ac
dionut711 <dionut711@yahoo.com>
2018-12-02 22:23:51 +0200

    padKey()

--- source.cpp
+++ source.cpp
@@ -2,6 +2,8 @@
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 const char * ERROR_MESSAGE_CTX = "Creating context failed.";
 const char * ERROR_MESSAGE_ENCRYPT_INIT = "EncryptInit failed.";
@@ -71,36 +73,56 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, const EVP_CIPHER* (*c
     return plaintext_length;
 }
 
+void encrypt_decrypt(unsigned char* plaintext, const EVP_CIPHER * (*cipher)(), unsigned char* key, unsigned char* iv)
+{
+    unsigned char ciphertext[128];
+    unsigned char decryptedtext[128];
+
+    int decryptedtext_len, ciphertext_len;
+
+    ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), cipher, key, iv, ciphertext);
+
+
+    printf("Ciphertext is:\n");
+    BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+
+
+    decryptedtext_len = decrypt(ciphertext, ciphertext_len, cipher, key, iv, decryptedtext);
+    decryptedtext[decryptedtext_len] = '\0';
+
+
+    printf("Decrypted text is:\n");
+    printf("%s\n", decryptedtext);
+}
+
+unsigned char* padKey(unsigned char* key, unsigned char character, int length)
+{
+    unsigned char* new_key = (unsigned char*)malloc(sizeof(unsigned char) * length);
+
+    int min_length = (strlen((char *)key) < length) ? strlen((char *)key) : length;
+    for (int i = 0; i < min_length; i++)
+        new_key[i] = key[i];
+    for (int i = min_length; i < length; i++)
+        new_key[i] = character;
+    new_key[length] = 0;
+
+    return new_key;
+}
+
 int main (void)
 {
+    unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+    unsigned char *iv = (unsigned char *)"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x0f";
+    const EVP_CIPHER* (*cipher)() = EVP_aes_256_ecb;
 
-  unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
-  // unsigned char *iv = (unsigned char *)"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x0f";
-  unsigned char *iv = (unsigned char *)"0123456789012345";
+    unsigned char *plaintext = (unsigned char *)"The quick brown fox jumps over the lazy dog";
 
+    //encrypt_decrypt(plaintext, cipher, key, iv);
+    char pad[] = "avadsfadsfasddf";
+    int fd = open("plaintext.txt", O_RDONLY);
+    char buffer[256];
+    int length = read(fd, buffer, sizeof(buffer));
+    close(fd);
 
-  unsigned char *plaintext = (unsigned char *)"The quick brown fox jumps over the lazy dog";
-
-  unsigned char ciphertext[128];
-  unsigned char decryptedtext[128];
-
-  int decryptedtext_len, ciphertext_len;
-
-  const EVP_CIPHER* (*cipher)() = EVP_aes_256_ecb;
-
-  ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), cipher, key, iv, ciphertext);
-
-
-  printf("Ciphertext is:\n");
-  BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
-
-
-  decryptedtext_len = decrypt(ciphertext, ciphertext_len, cipher, key, iv, decryptedtext);
-  decryptedtext[decryptedtext_len] = '\0';
-
-
-  printf("Decrypted text is:\n");
-  printf("%s\n", decryptedtext);
-
-  return 0;
+    return 0;
 }
